368997f7eba02d349bb8b5ba36f08c561a604906
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2019-02-27 03:25:05 -0600

    Add comments to Map Reduce code

--- src/main/scala/com/mayankrastogi/cs441/hw2/mapreduce/FacultyCollaborationDriver.scala
+++ src/main/scala/com/mayankrastogi/cs441/hw2/mapreduce/FacultyCollaborationDriver.scala
@@ -10,29 +10,50 @@ import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat, TextOutputFormat}
 
+/**
+  * Configures the map-reduce job and runs it.
+  */
 object FacultyCollaborationDriver extends LazyLogging {
+
+  // Load application settings
   private val settings = new Settings(ConfigFactory.load())
 
+  /**
+    * Runs the map-reduce job.
+    *
+    * @param args The first argument specifies the input directory. The second argument specifies the output directory.
+    */
   def main(args: Array[String]): Unit = {
     logger.trace(s"main(args: ${args.mkString})")
 
+    // Print error message if input and/or output paths are not provided as arguments
     if(args.length < 2) {
       logger.error("Input and/or output paths not provided.")
       System.exit(-1)
     }
+
+    // Run the map-reduce job
     runJob(args(0), args(1))
   }
 
+  /**
+    * Runs the map-reduce job.
+    *
+    * @param inputDir The location of input files for the map-reduce job.
+    * @param outputDir The location of where output of map-reduce job will be saved.
+    */
   def runJob(inputDir: String, outputDir: String): Unit = {
     logger.trace(s"runJob(inputPath: $inputDir, outputPath: $outputDir)")
 
     logger.debug("Configuring MapReduce Job...")
 
+    // Configure the start and end tags to search for in the XML file
     val conf = new Configuration()
 
     conf.setStrings(MultiTagXmlInputFormat.START_TAG_KEY, settings.xmlInputStartTags: _*)
     conf.setStrings(MultiTagXmlInputFormat.END_TAG_KEY, settings.xmlInputEndTags: _*)
 
+    // Configure the map-reduce job
     val job = Job.getInstance(conf, settings.jobName)
 
     job.setJarByClass(this.getClass)
@@ -44,12 +65,13 @@ object FacultyCollaborationDriver extends LazyLogging {
     job.setInputFormatClass(classOf[MultiTagXmlInputFormat])
     job.setOutputFormatClass(classOf[TextOutputFormat[Text, IntWritable]])
 
+    // Set input and output paths
     FileInputFormat.setInputPaths(job, new Path(inputDir))
 
     val outputPath = new Path(outputDir)
     FileOutputFormat.setOutputPath(job, outputPath)
 
-    // Clean output directory
+    // Clean output directory if it exists
     logger.trace("Removing any existing output files")
     val outputDirectoryDeleted = outputPath.getFileSystem(conf).delete(outputPath, true)
     if (outputDirectoryDeleted) {
@@ -59,6 +81,7 @@ object FacultyCollaborationDriver extends LazyLogging {
       logger.debug("Output path already clean")
     }
 
+    // Run the job and wait for its completion
     logger.info("Submitting job and waiting for completion...")
     job.waitForCompletion(true)
     logger.info("Job completed.")
--- src/main/scala/com/mayankrastogi/cs441/hw2/mapreduce/FacultyCollaborationReducer.scala
+++ src/main/scala/com/mayankrastogi/cs441/hw2/mapreduce/FacultyCollaborationReducer.scala
@@ -8,11 +8,16 @@ import org.apache.hadoop.mapreduce.Reducer
 
 import scala.collection.JavaConverters._
 
+/**
+  * Adds up all the values of the same key to get the final count of number of publications by each individual faculty
+  * and each faculty pair.
+  */
 class FacultyCollaborationReducer extends Reducer[Text, IntWritable, Text, IntWritable] with LazyLogging {
 
   override def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
     logger.trace(s"Reducer invoked: reduce(key: ${key.toString}, values: ${values.asScala.map(_.get())}")
 
+    // Sum up all the values for the same faculty pair
     val sum = values.asScala.fold(new IntWritable(0))((a, b) => new IntWritable(a.get() + b.get()))
     logger.info(s"Reducer emit: <$key, ${sum.get()}>")
     context.write(key, sum)
